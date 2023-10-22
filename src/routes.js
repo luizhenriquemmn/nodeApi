dc1806db7f384b23b5fac4760503d93a6a5c13af
Luiz Henrique <luizhenrique.mmn@gmail.com>
2023-10-22 07:45:17 -0400

    atualizando usuário

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
 
+import authMiddleware from './app/middlewares/auth'
+
+
+import UserControler from "./app/controllers/UserControler";
+import SessionController from "./app/controllers/SessionController";
+
 const routes = new Router();
 
-routes.get("/a", (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/users',UserControler.store);
+routes.post('/sessions',SessionController.store);
+
+routes.use(authMiddleware)
+routes.put('/users', UserControler.update);
+
+
 
 export default routes;
